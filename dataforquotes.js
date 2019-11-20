b62092f27070104e6e9f657db385d647032abbf0
Helina <helina.bajracharya@gmail.com>
2019-11-20 15:37:42 -0600

    Create dataforquotes.js

---
+++ dataforquotes.js
@@ -0,0 +1,39 @@
+[
+	{"id":1,
+	"quote":"You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth",
+	"name":"William W. Purkey",
+	"image":"http://images.freeimages.com/images/large-previews/46c/dance-1182283.jpg"
+	},
+	{
+		"id":2,
+		"quote":"It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
+		"name":"Friedrich Nietzsche",
+		"image":"http://images.freeimages.com/images/large-previews/66e/heart-in-the-sand-1307347.jpg"
+	},
+	{
+		"id":3,
+		"quote":"Never put off till tomorrow what may be done day after tomorrow just as well.",
+		"name":"Mark Twain",
+		"image":"http://images.freeimages.com/images/large-previews/27e/time-1196952.jpg"
+	},
+	{
+		"id":4,
+		"quote":"It’s no use going back to yesterday, because I was a different person then.",
+		"name":"Lewis Carroll",
+		"book":"Alice in Wonderland",
+		"image":"http://images.freeimages.com/images/large-previews/c12/adventure-in-the-mountain-1-1441989.jpg"
+	},
+	{
+		"id":5,
+		"quote":"I like work: it fascinates me. I can sit and look at it for hours.",
+		"name":"Jerome K. Jerome",
+		"image":"http://images.freeimages.com/images/large-previews/846/people-at-work-3-1459187.jpg"
+	},
+	{
+		"id":6,
+		"quote":"No one can make you feel inferior without your consent.",
+		"name":"Eleanor Roosevelt",
+		"book":"This is My Story ",
+		"image":"http://images.freeimages.com/images/large-previews/519/autumn-feelings-1188431.jpg"
+	}
+]
